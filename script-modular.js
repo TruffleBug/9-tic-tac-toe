e625920b26217579eea8a7b75421361b07a26307
TruffleBug <lisa.ho1022@gmail.com>
2024-10-25 15:26:12 -0600

    Finished product
    Everything should work. Couldn't get the winning mark to appear before
    the popup, but Clayton says it's because of the way html and JS interact
    and to not worry about it.

--- script-modular.js
+++ script-modular.js
@@ -1,7 +1,8 @@
 let tictactoe = (function(){
     // declare variables
+    // this.game = {};
+    // let game = this.game;
     let gameboardArray = Array.from(' '.repeat(9));
-    let allPlayerSelections = [];
     const winningCombos = [
         [0, 1, 2],
         [3, 4, 5],
@@ -13,27 +14,25 @@ let tictactoe = (function(){
         [2, 4, 6]
     ];
 
+
     // AUTOMATIC TASKS TO SET UP GAME
     // cache DOM (html query selectors)
     const gridButtons = document.querySelectorAll('.gameboard button');
-    const zero = document.querySelector('#zero');
-    const one = document.querySelector('#one');
-    const two = document.querySelector('#two');
-    const three = document.querySelector('#three');
-    const four = document.querySelector('#four');
-    const five = document.querySelector('#five');
-    const six = document.querySelector('#six');
-    const seven = document.querySelector('#seven');
-    const eight = document.querySelector('#eight');
     const startNewGameDialog = document.querySelector('.startNewGameDialog');
     const startNewGameButton = document.querySelector('.startNewGameButton');
     const playerInfoDialog = document.querySelector('.playerInfoDialog');
     const submitButton = document.querySelector('button[type="submit"]');
     const form = document.querySelector('form');
+    const playerNames = document.querySelector('.playerNames');
+    const restartButton = document.querySelector('.restartButton');
 
 
     // bind events (click event listeners)
     startNewGameButton.addEventListener('click', () => {
+        gridButtons.forEach((button) => {
+            button.textContent = ' ';
+            button.disabled = false;
+        });
         startNewGameDialog.close();
         playerInfoDialog.showModal();
     });
@@ -41,17 +40,37 @@ let tictactoe = (function(){
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         const formData = new FormData(form, submitButton);
-        let player1 = createPlayer(formData.get('player1'));
-        let player2 = createPlayer(formData.get('player2'));
+        this.player1 = createPlayer(formData.get('player1'));
+        this.player2 = createPlayer(formData.get('player2'));
+        playerNames.textContent = `${formData.get('player1')} vs. ${formData.get('player2')}`;
+        this.activePlayer = this.player1;
         form.reset();
         playerInfoDialog.close();
-        // return (player1, player2)
     })
 
-    // gridButtons.forEach((button) => {
-    //     // button.addEventListener('click', )
-    //     button.addEventListener('click', render())
-    // });
+    gridButtons.forEach((button) => {
+        // button.addEventListener('click', )
+        button.addEventListener('click', (e) => {
+            // console.log('activePlayer name', this.activePlayer.name)
+            let marker;
+            let winAlert = this.activePlayer.myTurn(e.target.id.slice(-1));
+            if (this.activePlayer.name == this.player1.name ) {
+                this.activePlayer = this.player2;
+                 marker = 'X';
+            } else {
+                this.activePlayer = this.player1;
+                marker = 'O';
+            };
+            button.textContent = marker;
+            button.disabled = true;
+            if (winAlert) {
+                alert(winAlert);
+            };
+            // checkForWin(activePlayer.name, activePlayer.playerSelections);
+        });
+    });
+
+    restartButton.addEventListener('click', (e) => gameReset())
 
     // START UP FUNCTIONS
     startNewGame();
@@ -60,67 +79,67 @@ let tictactoe = (function(){
     // FUNCTIONS GO UNDER HERE
     function startNewGame() {
         startNewGameDialog.showModal();
-    }
-
+    };
+    
     function render() {
-        console.log(gameboardArray);
+        // console.log({gameboardArray});
         // this.gridButtons.textContent = 'test';
     };
-
+    
     function createPlayer(name) {
         const newPlayer = Object.create(functionStore);
         newPlayer.name = name;
         newPlayer.playerSelections = [];
         return newPlayer
     };
-
+    
     const functionStore = {
         myTurn: function(index) {
-            if (!allPlayerSelections.includes(index)) {
-                this.playerSelections.push(index);
-                allPlayerSelections.push(index);
-                gameboardArray[index] = this.name;
-                render();
-                checkForWin(this.name, this.playerSelections);
-
-                console.log(this.name, this.playerSelections);
-                console.log('all', allPlayerSelections);
-            } else {
-                alert('That spot is taken. Try again');
-            };
+            this.playerSelections.push(Number(index));
+            gameboardArray[index] = this.name;
+            render();
+            let winAlert = checkForWin(this.name, this.playerSelections);
+            return winAlert
+            // console.log(this.name, this.playerSelections);
         }
     };
     
     function checkForWin(name, playerSelections) {
         // check for tie
         if(!gameboardArray.includes(' ')) {
+            render();
             alert('It\'s a tie!');
             gameReset();
         } else {
-            let checkForWin = [];
+            console.log({name})
+            console.log({playerSelections})
+            console.log({winningCombos})
+            let checkForWinArray = [];
+            console.log({checkForWinArray})
             winningCombos.forEach((combo1) => {
-                checkForWin.push(combo1.every((combo2) => playerSelections.includes(combo2)))});
-
-            console.log(checkForWin);
+                checkForWinArray.push(combo1.every((combo2) => playerSelections.includes(combo2)))});
             
-            if(checkForWin.includes(true)) {
-                alert(`${name} wins!`);
+            console.log({checkForWinArray});
+
+            if(checkForWinArray.includes(true)) {
+                let winAlert = `${name} wins!`;
                 gameReset();
+                return winAlert;
             };
         }
-    };
-
+        };
+        
     function gameReset() {
         gameboardArray = Array.from(' '.repeat(9));
-        allPlayerSelections = [];
         player1.playerSelections = [];
         player2.playerSelections = [];
         player1 = {};
         player2 = {};
+        this.activePlayer = this.player1;
         render();
+        startNewGame();
     };
-    
-
+            
     return {
         createPlayer
     }
@@ -128,3 +147,6 @@ let tictactoe = (function(){
 
 // let player1 = tictactoe.createPlayer('lisa');
 // let player2 = tictactoe.createPlayer('clayton');
+
+//DELETE RENDER SINCE IT'S ONLY CONSOLE LOGGING
+//IT'S PUSHING INTO WRONG PLAYERSELECTION ARRAY
